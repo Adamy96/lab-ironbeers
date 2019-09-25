ee3dda48bffddb041b5fe3a321495db197fb004f
Adamy <viniciusbohreradamy@gmail.com>
2019-09-25 20:33:41 -0300

    Initial commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -9,13 +8,37 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
-
+// ROUTES
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', {title: 'Homepage', css: 'stylesheets/absolute.css'});
+});
+
+app.get('/beers', async (req, res) => {
+  const data = await punkAPI.getBeers();
+  
+  try {
+    res.render('beers', {data, css: 'stylesheets/normalFlow.css'});
+    // res.send(data);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+app.get('/random-beers', async (req, res, next) => {
+  const beer = await punkAPI.getRandom();
+
+  try {
+    res.render("randomBeer", {
+      beer: beer[0],
+      css: 'stylesheets/absolute.css'
+    });
+  } catch(error) {
+    throw new Error(error);
+  }
 });
 
 
-
-app.listen(3000);
+app.listen(3000, () => console.log('Server online at port 3000.'));
